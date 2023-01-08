248a4f00db5eadcee6008d7e36b36476d23f681b
alessandradocouto <alessandradocouto@outlook.com>
2023-01-07 22:04:52 -0300

    add arquivos modificados

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from'react';
+//rotas
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+//pages
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+//componentes
+import Navbar from './components/Navbar';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </>
   );
 }
 
